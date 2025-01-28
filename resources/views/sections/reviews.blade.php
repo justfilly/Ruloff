03a60c313d408cf376063f0c9f86d236c0f06390
JustaFilly <mkhldvy@gmail.com>
2025-01-28 15:04:34 +0500

    Complete reviews styling

--- resources/views/sections/reviews.blade.php
+++ resources/views/sections/reviews.blade.php
@@ -1,21 +1,15 @@
 <section id="reviews" class="py-5">
     <div class="container">
         <h2 class="text-center mb-4">Отзывы наших клиентов</h2>
-        <div class="row g-4 justify-content-center">
-            <div class="col-md-4 col-sm-6">
-                <div class="review-item text-center">
-                    <img src="{{ asset('images/review_placeholder.png') }}" alt="Review 1" class="img-fluid rounded">
-                </div>
+        <div class="reviews-container">
+            <div class="review-item">
+                <img src="{{ asset('images/review_placeholder.png') }}" alt="Review 1" class="img-fluid rounded">
             </div>
-            <div class="col-md-4 col-sm-6">
-                <div class="review-item text-center">
-                    <img src="{{ asset('images/review_placeholder.png') }}" alt="Review 2" class="img-fluid rounded">
-                </div>
+            <div class="review-item">
+                <img src="{{ asset('images/review_placeholder.png') }}" alt="Review 2" class="img-fluid rounded">
             </div>
-            <div class="col-md-4 col-sm-6">
-                <div class="review-item text-center">
-                    <img src="{{ asset('images/review_placeholder.png') }}" alt="Review 3" class="img-fluid rounded">
-                </div>
+            <div class="review-item">
+                <img src="{{ asset('images/review_placeholder.png') }}" alt="Review 3" class="img-fluid rounded">
             </div>
         </div>
     </div>
