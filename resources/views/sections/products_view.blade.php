61ba9b9b2de0e8a012d4fb48262208f161a24632
JustaFilly <mkhldvy@gmail.com>
2025-01-28 14:41:04 +0500

    Complete products_view styling

--- resources/views/sections/products_view.blade.php
+++ resources/views/sections/products_view.blade.php
@@ -1,47 +1,47 @@
-<section id="products_view" class="py-5">
+<section id="products-view" class="py-5">
     <div class="container">
-        <h2 class="text-center mb-4">Наши продукты</h2>
+        <h2 class="text-center mb-4">Примеры работ</h2>
         <div class="row g-4">
             <div class="col-md-4">
                 <div class="product-card p-4 border text-center">
-                    <h3>Продукт 1</h3>
-                    <p>Описание продукта 1. Lorem ipsum dolor sit amet.</p>
-                    <p class="price fw-bold">5000 Р</p>
+                    <img src="{{ asset('images/product_placeholder.png') }}" alt="Продукт 1">
+                    <h3>Обтяжка руля для BMW X5</h3>
+                    <p>Качественная кожаная обтяжка руля для BMW X5, стильный и долговечный вариант.</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="product-card p-4 border text-center">
-                    <h3>Продукт 2</h3>
-                    <p>Описание продукта 2. Lorem ipsum dolor sit amet.</p>
-                    <p class="price fw-bold">7000 Р</p>
+                    <img src="{{ asset('images/product_placeholder.png') }}" alt="Продукт 2">
+                    <h3>Обтяжка руля для Audi A6</h3>
+                    <p>Роскошная отделка руля для Audi A6 с премиальной кожей для комфортного вождения.</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="product-card p-4 border text-center">
-                    <h3>Продукт 3</h3>
-                    <p>Описание продукта 3. Lorem ipsum dolor sit amet.</p>
-                    <p class="price fw-bold">6000 Р</p>
+                    <img src="{{ asset('images/product_placeholder.png') }}" alt="Продукт 3">
+                    <h3>Обтяжка руля для Mercedes-Benz E-Class</h3>
+                    <p>Идеально подходящая кожаная обтяжка для Mercedes-Benz E-Class с декоративными элементами.</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="product-card p-4 border text-center">
-                    <h3>Продукт 4</h3>
-                    <p>Описание продукта 4. Lorem ipsum dolor sit amet.</p>
-                    <p class="price fw-bold">5500 Р</p>
+                    <img src="{{ asset('images/product_placeholder.png') }}" alt="Продукт 4">
+                    <h3>Обтяжка руля для Toyota Camry</h3>
+                    <p>Персонализированная обтяжка руля для Toyota Camry с улучшенным grip.</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="product-card p-4 border text-center">
-                    <h3>Продукт 5</h3>
-                    <p>Описание продукта 5. Lorem ipsum dolor sit amet.</p>
-                    <p class="price fw-bold">7500 Р</p>
+                    <img src="{{ asset('images/product_placeholder.png') }}" alt="Продукт 5">
+                    <h3>Обтяжка руля для Ford Focus</h3>
+                    <p>Удобная и долговечная обтяжка руля для Ford Focus для максимального комфорта.</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="product-card p-4 border text-center">
-                    <h3>Продукт 6</h3>
-                    <p>Описание продукта 6. Lorem ipsum dolor sit amet.</p>
-                    <p class="price fw-bold">8000 Р</p>
+                    <img src="{{ asset('images/product_placeholder.png') }}" alt="Продукт 6">
+                    <h3>Обтяжка руля для Lexus RX</h3>
+                    <p>Элегантная и удобная кожаная обтяжка для руля Lexus RX с индивидуальным дизайном.</p>
                 </div>
             </div>
         </div>
