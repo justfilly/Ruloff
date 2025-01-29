bea6c862a1399e749dfe90213dd44c2740f04bb5
JustaFilly <mkhldvy@gmail.com>
2025-01-29 13:58:19 +0500

    Implement dynamic reviews

--- apache/ruloff/app/Http/Controllers/HomeController.php
+++ apache/ruloff/app/Http/Controllers/HomeController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\PriceItem;
-use App\Models\Product  ;
 use Illuminate\Http\Request;
+use App\Models\PriceItem;
+use App\Models\Product;
+use App\Models\Review;
 
 class HomeController extends Controller 
 {
@@ -13,7 +14,8 @@ class HomeController extends Controller
         $steeringWheelPrices = PriceItem::where('category', 'steering wheels')->get();
         $additionalServicesPrices = PriceItem::where('category', 'additional services')->get();
         $products = Product::all();
+        $reviews = Review::all();
 
-        return view('home', compact('steeringWheelPrices', 'additionalServicesPrices', 'products'));
+        return view('home', compact('steeringWheelPrices', 'additionalServicesPrices', 'products', 'reviews'));
     }
 }
--- apache/ruloff/resources/views/sections/reviews.blade.php
+++ apache/ruloff/resources/views/sections/reviews.blade.php
@@ -2,15 +2,11 @@
     <div class="container">
         <h2 class="text-center mb-4">Отзывы наших клиентов</h2>
         <div class="reviews-container">
-            <div class="review-item">
-                <img src="{{ asset('images/review_placeholder.png') }}" alt="Review 1" class="img-fluid rounded">
-            </div>
-            <div class="review-item">
-                <img src="{{ asset('images/review_placeholder.png') }}" alt="Review 2" class="img-fluid rounded">
-            </div>
-            <div class="review-item">
-                <img src="{{ asset('images/review_placeholder.png') }}" alt="Review 3" class="img-fluid rounded">
-            </div>
+            @foreach($reviews as $review)
+                <div class="review-item">
+                    <img src="{{ asset('storage/' . $review->image) }}" alt="Review" class="img-fluid rounded">
+                </div>
+            @endforeach
         </div>
     </div>
 </section>
