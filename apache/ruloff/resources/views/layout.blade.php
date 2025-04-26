485470ca576f206720c5eba50c9397b53cfde2c9
JustaFilly <mkhldvy@gmail.com>
2025-04-26 11:29:59 +0500

    Remove extra styles link

--- apache/ruloff/resources/views/layout.blade.php
+++ apache/ruloff/resources/views/layout.blade.php
@@ -22,7 +22,6 @@
         <script src="{{ asset('js/min.js') }}" defer></script>
     @endif
     
-    @stack('links')
 </head>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
