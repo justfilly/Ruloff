b0271445d7510659b0a63f4bcdfe8f3013808923
JustaFilly <mkhldvy@gmail.com>
2025-04-26 17:58:45 +0500

    Reorder scripts and links positions & mark scripts defer

--- apache/ruloff/resources/views/layout.blade.php
+++ apache/ruloff/resources/views/layout.blade.php
@@ -3,33 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>@yield('title')</title>
     <meta name="description" content="@yield('description')">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
     @if (app()->environment('local'))
         <link href="{{ asset('css/layout.css') }}" rel="stylesheet">
         <link href="{{ asset('css/header.css') }}" rel="stylesheet">
         <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
         @stack('links')
-        <script src="{{ asset('js/app.js') }}"></script>
     @else
         <link href="{{ asset('css/min.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/min.js') }}" defer></script>
     @endif
     
 </head>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
 <body>
     @include('sections.header')
     @yield('content')
     @include('sections.footer')
-    <script src="{{ asset('js/app.js') }}"></script>
+
+    @if (app()->environment('local'))
+        <script src="{{ asset('js/app.js') }}"></script>
+    @else
+        <script src="{{ asset('js/min.js') }}" defer></script>
+    @endif
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" defer></script>
 </body>
 </html>
