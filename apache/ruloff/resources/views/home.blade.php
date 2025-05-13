30cc7fabe37409c5bd77658d910e4ae43234f6c9
JustaFilly <mkhldvy@gmail.com>
2025-05-14 02:21:24 +0500

    Migrate frontend to vite

--- apache/ruloff/resources/views/home.blade.php
+++ apache/ruloff/resources/views/home.blade.php
@@ -3,14 +3,6 @@
     @section('title', 'Перетяжка руля в Нижнем Новгороде - Руль сервис 52')
     @section('description', 'Закажите качественную перетяжку руля в Нижнем Новгороде. Мы предлагаем экокожу, натуральную кожу, разнообразие цветов и нитей для вашего автомобиля. Быстро, профессионально, с гарантией.')    
 
-    @push('links')
-        <link rel="stylesheet" href="{{ asset('css/hero.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/prices.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/products.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/reviews.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/faq.css') }}">
-    @endpush
-
     @section('content')
         @include('sections.hero')
         @include('sections.prices')
--- apache/ruloff/vite.config.js
+++ apache/ruloff/vite.config.js
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        origin: 'http://localhost:5173',
+        cors: true,
+        watch: {
+            usePolling: true
+        }
+    },
+        build: {
+        manifest: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-});
+})
