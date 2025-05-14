01a00b0ea058cf81129baa582514c2265c66f476
JustaFilly <mkhldvy@gmail.com>
2025-05-15 03:55:39 +0500

    Implement with GLightBox

--- apache/ruloff/resources/js/app.js
+++ apache/ruloff/resources/js/app.js
@@ -1,3 +1,4 @@
 import '@/modules/form/input/formController'
 import '@/modules/slider/sliderController'
 import { showToast } from '@/modules/toast/toastController.js';
+import '@/modules/images-zoom-view/ImagesZoomView'
