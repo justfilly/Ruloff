f92eb9e0e688af532aa4c2907a41c00d28cfa34a
JustaFilly <mkhldvy@gmail.com>
2025-05-14 23:22:18 +0500

    Remove cache control headers from routes

--- apache/ruloff/routes/web.php
+++ apache/ruloff/routes/web.php
@@ -8,15 +8,8 @@ use TCG\Voyager\Facades\Voyager;
 
 Route::get('voyager/orders/export', [OrderExportController::class, 'export'])->name('voyager.orders.export');
 
-Route::get('/', function () {
-    $response = app(HomeController::class)->index(request());
-    return response($response)->header('Cache-Control', 'public, max-age=86400');
-})->name('home');
-
-Route::post('/orders', function () {
-    $response = app(OrderController::class)->store(request());
-    return response($response)->header('Cache-Control', 'no-store');
-})->name('order.store');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::post('/orders', [OrderController::class, 'store'])->name('order.store');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
