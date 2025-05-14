644b4bd910339d6b704885775b1f96be5f5e9f91
JustaFilly <mkhldvy@gmail.com>
2025-05-15 04:14:23 +0500

    Revert "Implement with LightGallery"
    This reverts commit 3a352ef3333957a4cb7f36514495bcaca8440213.

--- apache/ruloff/resources/js/app.js
+++ apache/ruloff/resources/js/app.js
@@ -1,4 +1,3 @@
 import '@/modules/form/input/formController'
 import '@/modules/slider/sliderController'
 import { showToast } from '@/modules/toast/toastController.js';
-import '@/modules/images-zoom-view/ImagesZoomView'
--- apache/ruloff/resources/views/sections/products.blade.php
+++ apache/ruloff/resources/views/sections/products.blade.php
@@ -2,25 +2,21 @@
     <div class="container">
         <h3 class="text-center mb-4">Примеры работ</h3>
         
-        <div id="product-gallery">
-            <div class="swiper mySwiper">
-                <div class="swiper-wrapper">
-                    @foreach($products as $product)
-                        <div class="swiper-slide">
-                            <div class="product-card">
-                                <a href="{{ asset('storage/' . $product->image) }}" class="zoomable">
-                                    <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" loading="lazy">
-                                </a>
-                                <h4>{{ $product->name }}</h4>
-                                <p>{{ $product->description }}</p>
-                            </div>
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                @foreach($products as $product)
+                    <div class="swiper-slide">
+                        <div class="product-card">
+                            <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" loading="lazy">
+                            <h4>{{ $product->name }}</h4>
+                            <p>{{ $product->description }}</p>
                         </div>
-                    @endforeach
-                </div>
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
+                    </div>
+                @endforeach
             </div>
-        </div>
 
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
     </div>
 </section>
