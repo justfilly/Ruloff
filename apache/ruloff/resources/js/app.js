eb4bcca8486b75dd058a52e9daf9e33467c9e1e0
JustaFilly <mkhldvy@gmail.com>
2025-05-15 11:44:37 +0500

    Add map section with yandex maps vidget

--- apache/ruloff/resources/js/app.js
+++ apache/ruloff/resources/js/app.js
@@ -2,3 +2,4 @@ import '@/modules/form/input/formController'
 import '@/modules/slider/sliderController'
 import { showToast } from '@/modules/toast/toastController.js';
 import '@/modules/images-zoom-view/ImagesZoomView'
+import '@/modules/map/map'
---
+++ apache/ruloff/resources/views/sections/map.blade.php
@@ -0,0 +1,8 @@
+<section id="map" class="py-5">
+    <div class="container">
+        <h3 class="text-center mb-4">Мы на карте</h3>
+        <div id="yandex-map" style="width: 100%; height: 400px;"></div>
+    </div>
+</section>
+
+<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
